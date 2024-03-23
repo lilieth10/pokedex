a91c7f1ee8cc07bacc6a1d917de26f731a6834bd
lilieth <liliethramirez91@gmail.com>
2024-03-23 19:08:25 -0300

    se modularizo el codigo para mejor compresion

--- src/services/pokedex.js
+++ src/services/pokedex.js
@@ -2,7 +2,7 @@
 
 const BASE_URL = 'https://pokeapi.co/api/v2/';
 
-async function getPokemonList(offset = 0, limit = 20) {
+async function getPokemonList(offset = 0, limit = 30) {
     const response = await fetch(`${BASE_URL}pokemon?offset=${offset}&limit=${limit}`);
     const data = await response.json();
     return data.results;
