20a8ff2ba6439393e088ff922e8d6441f2d77aba
lilieth <liliethramirez91@gmail.com>
2024-03-25 22:31:47 -0300

    pruebas jest

--- src/services/pokedex.js
+++ src/services/pokedex.js
@@ -5,6 +5,7 @@ async function getPokemonList(offset = 0, limit = 30) {
     const response = await fetch(`${BASE_URL}pokemon?offset=${offset}&limit=${limit}`);
     const data = await response.json();
     return data.results;
+  
 }
 
 async function getPokemonDetails(name) {
--- src/ui/index.js
+++ src/ui/index.js
@@ -1,3 +1,4 @@
+// index.js
 import { getPokemonList, getPokemonDetails } from '../services/pokedex.js';
 import { avanzarPagina, retrocederPagina } from '../ui/paginado.js';
 
@@ -5,13 +6,12 @@ let currentPage = 1;
 const limit = 10;
 
 async function showPokemonList(offset) {
-    console.log("Mostrando lista de Pokémon...");
     const pokemonList = await getPokemonList(offset, limit);
     const pokemonContainer = document.getElementById('pokemones');
-
+    
     pokemonContainer.innerHTML = '';
 
-    pokemonList.forEach(async pokemon => {
+    pokemonList.forEach(pokemon => {
         const pokemonElement = document.createElement('div');
         pokemonElement.textContent = pokemon.name;
         pokemonElement.addEventListener('click', async () => {
@@ -28,7 +28,6 @@ async function showPokemonDetails(pokemonDetails) {
     const detailContainer = document.getElementById('detalle');
     detailContainer.innerHTML = ''; 
 
-    
     const detailImage = document.createElement('img');
     detailImage.src = pokemonDetails.sprites.front_default; 
     detailImage.alt = pokemonDetails.name;
@@ -51,33 +50,44 @@ async function showPokemonDetails(pokemonDetails) {
     detailContainer.appendChild(detailHeight);
 }
 let timeout;
-
-document.getElementById('boton-busqueda').addEventListener('click', async () => {
-    clearTimeout(timeout);  
-    const searchValue = document.getElementById('barra-busqueda').value.toLowerCase();
-
-    if (searchValue) {
-        const pokemon = await getPokemonDetails(searchValue);
-        if (pokemon) {
-            showPokemonDetails(pokemon);
+function setupEventListeners() {
+    const botonBusqueda = document.getElementById('boton-busqueda');
+    if (botonBusqueda) {
+        botonBusqueda.addEventListener('click', async () => {
+            clearTimeout(timeout);
+            const searchValue = document.getElementById('barra-busqueda').value.toLowerCase();
+            if (searchValue) {
+                const pokemon = await getPokemonDetails(searchValue);
+                if (pokemon) {
+                    showPokemonDetails(pokemon);
+                } else {
+                    alert('¡Pokémon no encontrado!');
+                }
             } else {
-                alert('¡Pokémon no encontrado!');
+                document.getElementById('detalle').innerHTML = '';
             }
-        } else {
-            
-            document.getElementById('detalle').innerHTML = '';
-        }
-    }, 500); 
+        });
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    setupEventListeners();
     showPokemonList(0);
 });
 
-document.getElementById('btnNext').addEventListener('click', async () => {
-    avanzarPagina(() => showPokemonList(currentPage * limit));
-    currentPage++;
-});
+const btnNext = document.getElementById('btnNext');
+if (btnNext) {
+    btnNext.addEventListener('click', async () => {
+        avanzarPagina(() => showPokemonList(currentPage * limit));
+        currentPage++;
+    });
+}
 
-document.getElementById('btnPrev').addEventListener('click', async () => {
-    currentPage--;
-    retrocederPagina(() => showPokemonList((currentPage - 1) * limit));
-});
+const btnPrev = document.getElementById('btnPrev');
+if (btnPrev) {
+    btnPrev.addEventListener('click', async () => {
+        currentPage--;
+        retrocederPagina(() => showPokemonList((currentPage - 1) * limit));
+    });
+}
+export { showPokemonList, showPokemonDetails  };
