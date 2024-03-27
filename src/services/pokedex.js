7ab7597d47f579dc66bb2731556a14f608235aa4
lilieth <liliethramirez91@gmail.com>
2024-03-26 21:38:37 -0300

    seagrego la clase pokemon

--- src/services/pokedex.js
+++ src/services/pokedex.js
@@ -1,17 +1,31 @@
+import Pokemon from "../clases/pokemon.js";
 
 const BASE_URL = 'https://pokeapi.co/api/v2/';
 
 async function getPokemonList(offset = 0, limit = 30) {
     const response = await fetch(`${BASE_URL}pokemon?offset=${offset}&limit=${limit}`);
     const data = await response.json();
-    return data.results;
+    
+    const pokemonList = data.results.map(pokemonData => {
+        return new Pokemon(pokemonData.name);
+    });
+    return pokemonList;
   
 }
 
 async function getPokemonDetails(name) {
     const response = await fetch(`${BASE_URL}pokemon/${name}`);
     const data = await response.json();
-    return data;
+    
+    const pokemon = new Pokemon(
+        data.name,
+        data.sprites,
+        data.base_experiencie,
+        data.weight,
+        data.height
+
+    );
+    return pokemon;
 }
 
 export { getPokemonList, getPokemonDetails };
